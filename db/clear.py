6ee3f8220b8435ad616cf4b6f7c4ecc86f38a680
Jarrod Stone <41911539+JarrodStone-Altairix@users.noreply.github.com>
2019-09-09 08:59:00 -0400

    cleanup

--- db/clear.py
+++ db/clear.py
@@ -1,4 +1,4 @@
-import ix.db.ara as aradb;
+import ix.db.ara as aradb
 
 
 def clear_recursively(uid_name, uid_value, root, *children):
@@ -7,17 +7,15 @@ def clear_recursively(uid_name, uid_value, root, *children):
   cursor = db.cursor()
 
   for child in children:
-    cursor.execute(f"""DELETE from altairix.{child} where {uid_name}='{uid_value}'""")
-    print (f"Table - {child}: {cursor.rowcount} row(s) affected.")
+    cursor.execute(
+        f"""DELETE from altairix.{child} where {uid_name}='{uid_value}'""")
+    print(f"Table - {child}: {cursor.rowcount} row(s) affected.")
 
-  cursor.execute(f"""DELETE from altairix.{root} where {uid_name}='{uid_value}'""")
-  print (f"Table - {root}: {cursor.rowcount} row(s) affected.")
+  cursor.execute(
+      f"""DELETE from altairix.{root} where {uid_name}='{uid_value}'""")
+  print(f"Table - {root}: {cursor.rowcount} row(s) affected.")
 
   db.commit()
 
   cursor.close()
   db.close()
-
-
-# clear_recursively("QNAIRE_UID", "000000001U", "ixaqnaire", "ixaqquest", "ixaqans")
-
